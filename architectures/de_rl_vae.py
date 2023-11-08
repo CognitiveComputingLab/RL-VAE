644df5704bd2bccf8f16176c380a76c24af02ab9
Mark Jacobsen <mark.jacobsen2003@gmail.com>
2023-11-08 16:14:12 +0000

    changed to noisy circle data, implemented decreasing exploration rl-vae

---
+++ architectures/de_rl_vae.py
@@ -0,0 +1,25 @@
+import torch
+import architectures.rl_vae as rl_vae
+
+
+class DecreasingExplorationRLVAE(rl_vae.RlVae):
+    def __init__(self, device, input_dim, latent_dimensions=2):
+        super().__init__(device, input_dim, latent_dimensions)
+        self.arch_name = "DecreasingExplorationRL-VAE"
+        self.encoder_agent = rl_vae.MeanEncoderAgent(input_dim, latent_dimensions).to(self.device)
+        self.exploration_function = self.decreasing_exploration_function
+        self.reward_function = self.non_exploration_reward_function
+
+        self.initial_exploration = 2
+        self.exploration_decay = 0.99
+        self.min_exploration = 0.1
+        self.current_exploration = self.initial_exploration
+
+    def decreasing_exploration_function(self):
+        """
+        set the exploration rate to a constant value across all dimensions
+        """
+        logvar = torch.tensor([self.current_exploration] * self.latent_dimensions).to(self.device)
+        self.current_exploration *= self.exploration_decay
+        self.current_exploration = max(self.min_exploration, self.current_exploration)
+        return logvar
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from toy_data import data
 from toy_data import plotting
 from toy_data import embedding
 
-from architectures import rl_vae, vae, ce_rl_vae
+from architectures import rl_vae, vae, ce_rl_vae, de_rl_vae
 import helper
 
 # module init
@@ -12,13 +12,18 @@ torch.manual_seed(0)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("using device: ", device)
 
+
 if __name__ == "__main__":
     # generate data to be embedded
-    toy_data = data.MoebiusStrip(n=10000, width=1, turns=6)
+    # toy_data = data.MoebiusStrip(n=10000, width=1, turns=6)
+    toy_data = data.Circle2D(n=1000)
     toy_data.generate()
+    toy_data.add_noise()
+    input_dim = toy_data.data.shape[1]
 
     # plot the data
-    fig = plotting.scatter3d(toy_data)
+    # plotting.mpl_2d_plot(toy_data)
+    # fig = plotting.scatter3d(toy_data)
     # fig.show()
 
     # use UMAP embedding
@@ -27,15 +32,14 @@ if __name__ == "__main__":
     # umap.plot()
 
     # train RL-VAE system on data
-    input_dim = 3
-    model = rl_vae.RlVae(device, input_dim)
+    model = de_rl_vae.DecreasingExplorationRLVAE(device, input_dim)
     toy_dataset = helper.ToyTorchDataset(toy_data)
     data_loader = torch.utils.data.DataLoader(
         toy_dataset,
         batch_size=128,
         shuffle=False
     )
-    model.train(data_loader, epochs=5)
+    model.train(data_loader, epochs=100)
     model.plot_latent(data_loader, f"images/{model.arch_name}-latent.png")
     model.plot_loss(f"images/{model.arch_name}-loss.png")
 
