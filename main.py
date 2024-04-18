fixed gumbel softmax k head vae

main.py
@@ -20,13 +20,14 @@ class Main:
         self.reward_history = []
 
     def train(self, epochs, latent_freq=10):
-        optimizer = torch.optim.Adam(list(self.emb_model.parameters()))
+        optimizer = torch.optim.Adam(list(self.emb_model.parameters()), lr=0.001)
         for epoch in tqdm(range(epochs), disable=False):
             self.emb_model.sampler.reset_epoch()
 
             # run through epoch
             epoch_done = False
             epoch_reward = 0
+            frame = 0
             while not epoch_done:
                 reward, epoch_done = self.emb_model(epoch)
                 loss = -reward[self.emb_model.reward_name]
@@ -36,11 +37,15 @@ class Main:
                 loss.backward()
                 optimizer.step()
 
+                """frame += 1
+                if frame == 5:
+                    return"""
+
             self.reward_history.append(epoch_reward)
 
             if not latent_freq == 0 and epoch % latent_freq == 0:
                 self.plot_latent(f"images/latent-{epoch}.png")
-                print(self.emb_model.explorer.current_exploration)
+                print(self.emb_model.explorer.epsilon)
 
     def plot_latent(self, path):
         # init
@@ -109,7 +114,7 @@ if __name__ == "__main__":
     toy_dataset = toy_torch_dataset.ToyTorchDataset(toy_data)
     data_loader = torch.utils.data.DataLoader(
         toy_dataset,
-        batch_size=10,
+        batch_size=100,
         shuffle=False
     )
 
@@ -117,8 +122,8 @@ if __name__ == "__main__":
     # model = examples.UMAP(3, 2, device, data_loader)
     # model = examples.TSNE(3, 2, device, data_loader)
     # model = examples.VAE(3, 2, device, data_loader)
-    model = examples.VarianceVAEDecreasing(3, 2, device, data_loader)
-    # model = examples.KHeadVAEDecreasing(3, 2, device, data_loader, k=5)
+    # model = examples.VarianceVAEDecreasing(3, 2, device, data_loader)
+    model = examples.KHeadVAEDecreasing(3, 2, device, data_loader, k=10)
     # model.explorer.current_exploration = 0
     model.reward.success_weight = 100
     # model.reward.kl_weight = 0
@@ -135,7 +140,7 @@ if __name__ == "__main__":
 
     # train the model
     # TODO multi head not working! torch.where gradients?
-    m.train(epochs=1000, latent_freq=20)
+    m.train(epochs=5000, latent_freq=100)
     m.plot_latent(f"images/post-training.png")
     m.plot_reward(f"images/reward-history.png")
 

rl_embeddings/encoders.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as f
 from rl_embeddings.components import Component
@@ -194,7 +195,7 @@ class EncoderKHeadVAE(nn.Module, Component):
         self.linearM = nn.Linear(4096, k * latent_dims)
         self.linearS = nn.Linear(4096, k * latent_dims)
         self.weight_gm = GeneralModel(input_dim, [1024, 2048, 2048, 4096])
-        self.linear_weight = nn.Linear(4096, k)
+        self.linear_weight = nn.Linear(3, k)
 
     def forward(self, **kwargs):
         """
@@ -224,8 +225,9 @@ class EncoderKHeadVAE(nn.Module, Component):
         batch_size = logvar.size(0)
         logvar = logvar.view(batch_size, self.k, -1)
 
-        weights = self.weight_gm(x)
-        weights = self.linear_weight(weights)
+        # weights = self.weight_gm(x)
+        weights = self.linear_weight(x)
         # weights = nn.functional.softmax(weights, dim=1)
+        # weights = torch.FloatTensor([[1]]*x.shape[0]).to(logvar.device)
 
         return {"head_means": mu, "head_log_vars": logvar, "head_weights": weights}

rl_embeddings/explorers.py
@@ -58,9 +58,9 @@ class ExplorerKHeadVAE(Explorer):
         self._required_inputs = ["epoch", "head_means", "head_log_vars", "head_weights"]
 
         # hyperparameters
-        self.min_epsilon = 0.0
+        self.min_epsilon = 0.01
         self.decay_rate = 0.999
-        self.epsilon_start = 0.0
+        self.epsilon_start = 2
 
         # exploration tracking
         self.epsilon = self.epsilon_start
@@ -72,7 +72,7 @@ class ExplorerKHeadVAE(Explorer):
         if not mode:
             # no exploration during evaluation
             self.epsilon_save = self.epsilon
-            self.epsilon = 0
+            self.epsilon = 0.001
         # training mode
         else:
             # activate exploration
@@ -108,25 +108,16 @@ class ExplorerKHeadVAE(Explorer):
         if epoch:
             self.exploration_function(epoch)
 
-        batch_size, num_choices = weight.shape
-
-        # choose points in batch based on probability
-        random_selection_mask = torch.rand(batch_size, device=self._device) < self.epsilon
-
         # get max weight index for each point in batch
-        gumbel_softmax_indices = f.gumbel_softmax(weight, tau=1, hard=True, dim=1)
+        gumbel_softmax_indices = f.gumbel_softmax(weight, tau=self.epsilon, hard=True, dim=1)
 
-        # generate random one-hot encoded selections for random indices
-        random_indices = torch.randint(0, num_choices, (batch_size,), device=self._device)
-        random_one_hot = f.one_hot(random_indices, num_classes=num_choices).to(dtype=weight.dtype, device=self._device)
-
-        # combine gumbel_softmax selection with random selection based on mask
-        chosen_one_hot = torch.where(random_selection_mask.unsqueeze(-1), random_one_hot, gumbel_softmax_indices)
+        # get weights
+        chosen_weights = torch.sum(gumbel_softmax_indices * weight, dim=1)
 
         # get chosen mu and log_var by multiplying with one hot choices
-        chosen_mu = torch.sum(chosen_one_hot.unsqueeze(2) * mu, dim=1)
-        chosen_log_var = torch.sum(chosen_one_hot.unsqueeze(2) * log_var, dim=1)
-        chosen_weights = torch.sum(chosen_one_hot * weight, dim=1)
+        gumbel_softmax_indices = gumbel_softmax_indices.view(-1, gumbel_softmax_indices.size(1), 1)
+        chosen_mu = torch.sum(gumbel_softmax_indices * mu, dim=1)
+        chosen_log_var = torch.sum(gumbel_softmax_indices * log_var, dim=1)
 
         # re-parameterize
         # compute the standard deviation
@@ -135,7 +126,6 @@ class ExplorerKHeadVAE(Explorer):
         eps = torch.randn_like(std)
         # generate a sample
         sample = chosen_mu + std * eps
-        # print("sample: ", sample)
 
         return {"encoded_points": sample, "chosen_weights": chosen_weights, "chosen_means": chosen_mu,
                 "chosen_log_vars": chosen_log_var}

rl_embeddings/reward_calculators.py
@@ -105,12 +105,9 @@ class RewardCalculatorKHeadVAE(RewardCalculator):
         chosen_log_var = kwargs["chosen_log_vars"]
 
         chosen_weights = chosen_weights.view(chosen_weights.shape[0], 1)
+        chosen_weights = f.softmax(chosen_weights, dim=0)
 
-        # print("chosen weights: ", chosen_weights)
-        # print("chosen means: ", chosen_mu)
-        # print("chosen log_var", chosen_log_var)
-
-        # similar to VAE loss, without KL term
+        # similar to VAE loss, without KL term and weighted by weight output
         variance = torch.exp(chosen_log_var)
         surprise = variance + torch.square(chosen_mu)
         success = f.mse_loss(x_a, x_b)